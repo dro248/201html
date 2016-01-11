5e60bd4f34f190013eeb321a71fbb67ba8eb6fe4
dro248 <david.ostler001@gmail.com>
2016-01-11 01:20:48 -0700

    updates

--- scripts.js
+++ scripts.js
@@ -9,7 +9,11 @@ function makeImageFullScreen() {
 
 	myImg = document.getElementById("home1");
 
-	if(window.innerWidth / window.innerHeight > 1.5) {
+	if(window.innerHeight > window.innerWidth){
+		myImg.style.height = window.innerHeight+ "px";
+	}
+
+	else if(window.innerWidth / window.innerHeight > 1.5) {
 		myImg.style.width = window.innerWidth+ "px";
 		myImg.style.height = window.innerWidth/1.5 + "px";
 	}
@@ -28,4 +32,11 @@ function fixTopMargin() {
     var imgHeight = document.getElementById("home1").height;
 
     document.getElementById("Education").style.marginTop = imgHeight + "px";
+}
+
+function resumeButton() {
+	var resumeButton = document.getElementById("floatingLink");
+	var myImg = document.getElementById("home1");
+
+	resumeButton.top = (myImg.height - 20) + "px";
 }
