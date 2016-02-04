c31fde7ec8a0bdcb511e7589c013a9440224456f
Santiago Verdu <santiago.verdu.01@gmail.com>
2016-02-03 19:38:03 -0700

    nytimes

--- Class_Projects/dashboard/js/scripts.js
+++ Class_Projects/dashboard/js/scripts.js
@@ -16,5 +16,18 @@ $(function() {
 			$("#header").html(weather);
 		});
 	}
-	weather()
+
+	function nyTimes() {
+		var sections = [ "home", "world", "national", "politics", "nyregion", "business", "opinion", "technology", 
+			"science", "health", "sports", "arts", "fashion", "dining", "travel", "magazine", "realestate" ],
+		topStoriesKey = "bf1c3b8bf43dcf7910aae43bf75d25f1:8:74283828",
+		newswireKey = "a8cf6af2d1820b864378acd5c9b9ca1b:8:74283828",
+		url = "http://api.nytimes.com/svc/topstories/v1/"+sections[2]+".jsonp?api-key="+newswireKey;
+		$.getJSON(url, function(data) {
+			console.log(data);
+		});
+	}
+
+	nyTimes()
+
 });
