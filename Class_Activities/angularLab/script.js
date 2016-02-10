c86c374f5c09ca5f063943a9c7e5fac43698107f
dro248 <david.ostler001@gmail.com>
2016-02-10 00:01:35 -0700

    adding angularLab

---
+++ Class_Activities/angularLab/script.js
@@ -0,0 +1,45 @@
+/**
+ * 1. We have added a directive with the name 'avatar' and handler of
+ * avatarDirective to our angular app module
+ */
+angular.module('app', [])
+  .controller('mainCtrl', mainCtrl)
+  .directive('avatar', avatarDirective);
+
+function mainCtrl ($scope) {
+
+  $scope.users = [];
+
+  $scope.addNew = function (user) {
+    $scope.users.push({ 
+      name: user.name,
+      avatarUrl: user.url
+    });
+    
+    user.name = '';
+    user.url = '';
+  };
+}
+
+function avatarDirective () {
+  return {
+    scope: {
+      user: '='
+    },
+    restrict: 'E',
+    template: (
+      '<div class="Avatar">' +
+        '<img ng-src="{{user.avatarUrl}}" />' +
+        '<h4>{{user.name}}</h4>' +
+      '</div>'
+    ),
+    link: link
+  };
+  
+  function link (scope) {
+    if (!scope.user.avatarUrl) {
+      scope.user.avatarUrl = 'http://thealmanac.org/assets/img/default_avatar.png';
+    }
+  }
+
+}
